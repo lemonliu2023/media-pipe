1b02b83e5fbf4addf64ef52824c24f1bccbeeaa9
刘明 <mingliu63@creditease.cn>
2025-04-22 18:58:36 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import { useEffect, useRef, useState } from 'react';
 import { PoseLandmarker, FilesetResolver } from '@mediapipe/tasks-vision';
 import Predict from './Predict';
 
-const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
-
 function App() {
   const [loadingModel, setLoadingModel] = useState(false);
   const poseLandmarkerRef = useRef<PoseLandmarker>(null);
 
   useEffect(() => {
-    console.log(hasGetUserMedia());
     setLoadingModel(true);
     const createPoseLandmarker = async () => {
       const vision = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm');
@@ -19,7 +16,7 @@ function App() {
           delegate: 'GPU',
         },
         runningMode: 'VIDEO',
-        numPoses: 2,
+        numPoses: 1, // 设置同时检测的最大姿态数量
       });
     };
     createPoseLandmarker().then(() => {
@@ -29,7 +26,7 @@ function App() {
 
   return (
     <div className="flex justify-center items-center h-screen overflow-hidden">
-      {loadingModel ? <div>模型加载中...</div> : <Predict width={1280} height={720} poseLandmarkerRef={poseLandmarkerRef} />}
+      {loadingModel ? <div>模型加载中...</div> : <Predict poseLandmarkerRef={poseLandmarkerRef} />}
     </div>
   );
 }
--- src/Predict.tsx
+++ src/Predict.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 
 let lastVideoTime = -1;
 
-function Predict({ poseLandmarkerRef }: { width: number; height: number; poseLandmarkerRef: React.RefObject<PoseLandmarker | null> }) {
+function Predict({ poseLandmarkerRef }: { poseLandmarkerRef: React.RefObject<PoseLandmarker | null> }) {
   const [enableCamera, setEnableCamera] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -27,53 +27,68 @@ function Predict({ poseLandmarkerRef }: { width: number; height: number; poseLan
           });
           drawingUtilsRef.current?.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
         }
-        console.log(canvasCtxRef.current, '####');
         canvasCtxRef.current?.restore();
       });
     }
 
-    // Call this function again to keep predicting when the browser is ready.
-    // if (webcamRunning === true) {
     window.requestAnimationFrame(predictWebcam);
-    // }
   }
 
-  function enableCamHandler() {
-    if (!poseLandmarkerRef.current) {
-      alert('Wait! poseLandmaker not loaded yet.');
-      return;
-    }
+  function getCameraStream(): Promise<MediaStream> {
+    return new Promise((res, rej) => {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia({
+            video: {
+              facingMode: 'user', // 前置摄像头，'environment' 为后置
+              width: { ideal: 1280 }, // 理想分辨率
+              height: { ideal: 720 },
+            },
+            audio: false,
+          })
+          .then((stream) => res(stream));
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+      } else if (navigator.getUserMedia) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        navigator.getUserMedia(
+          { video: true, audio: false },
+          (stream: MediaStream) => res(stream),
+          (err: Error) => rej(err.message)
+        );
+      } else {
+        rej('Your browser does not support getUserMedia API');
+      }
+    });
+  }
 
+  async function enableCamHandler() {
     // Activate the webcam stream.
-    navigator.mediaDevices
-      .getUserMedia({
-        video: {
-          width: { ideal: 1280 }, // 建议宽度
-          height: { ideal: 720 }, // 建议高度
-          facingMode: 'user', // 前置摄像头
-        },
-      })
-      .then((stream) => {
-        if (videoRef.current) {
-          setEnableCamera(true);
-          videoRef.current.srcObject = stream;
-          videoRef.current.addEventListener('loadeddata', () => {
-            const { videoWidth, videoHeight } = videoRef.current!;
-            setSize({
-              width: videoWidth,
-              height: videoHeight,
-            });
-            setTimeout(() => {
-              canvasCtxRef.current = canvasRef.current!.getContext('2d');
-              drawingUtilsRef.current = new DrawingUtils(canvasCtxRef.current!);
-              predictWebcam();
-            });
+    try {
+      const stream = await getCameraStream();
+      if (videoRef.current) {
+        setEnableCamera(true);
+        videoRef.current.srcObject = stream;
+        videoRef.current.addEventListener('loadeddata', () => {
+          const { videoWidth, videoHeight } = videoRef.current!;
+          setSize({
+            width: videoWidth,
+            height: videoHeight,
           });
-        }
-      });
+          setTimeout(() => {
+            canvasCtxRef.current = canvasRef.current!.getContext('2d');
+            drawingUtilsRef.current = new DrawingUtils(canvasCtxRef.current!);
+            predictWebcam();
+          });
+        });
+      }
+    } catch (error) {
+      alert(error);
+    }
   }
   return (
-    <div className="relative flex justify-center items-center" style={{ width: 1280, height: 720 }}>
+    <div className="relative flex justify-center items-center" style={{ width: size.width, height: size.height }}>
       {!enableCamera && <button onClick={() => enableCamHandler()}>enable camera</button>}
       <video
         ref={videoRef}
@@ -81,7 +96,11 @@ function Predict({ poseLandmarkerRef }: { width: number; height: number; poseLan
         autoPlay
         width={size.width}
         height={size.height}
-        style={{ display: enableCamera ? 'block' : 'none', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+        style={{
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%) scaleX(-1)', // 添加scaleX(-1)实现镜像
+        }}
         className="absolute"
       ></video>
       <canvas
@@ -89,7 +108,11 @@ function Predict({ poseLandmarkerRef }: { width: number; height: number; poseLan
         width={size.width}
         height={size.height}
         ref={canvasRef}
-        style={{ display: enableCamera ? 'block' : 'none', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' /* 居中 */ }}
+        style={{
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%) scaleX(-1)', // 同时镜像canvas
+        }}
         className="absolute"
       ></canvas>
     </div>
