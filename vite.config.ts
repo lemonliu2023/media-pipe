071179bdec167ca5993bedc3d6f06cd6c7bdda4e
刘明 <mingliu63@creditease.cn>
2025-04-22 14:27:52 +0800

    feat: add base outDir config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/media-pipe',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [tailwindcss(), react()],
 });
