3aecb3247bced2a30acad358b6d7ae109672ef21
刘明 <mingliu63@creditease.cn>
2025-04-24 17:38:17 +0800

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/media-pipe',
+  assetPrefix: '/media-pipe',
   output: 'export',
   distDir: 'docs'
 };
