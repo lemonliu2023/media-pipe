44d57f80e341850da237fbbac42a13edb122bff2
刘明 <mingliu63@creditease.cn>
2025-04-24 18:56:05 +0800

    feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { useEffect, useRef, useState } from 'react';
-import DeepSquat from './DeepSquat';
+import DeepSquat from './components/DeepSquat';
 import { FilesetResolver, PoseLandmarker } from '@mediapipe/tasks-vision';
 
 export default function Home() {
-  const [loadingModel, setLoadingModel] = useState(false);
+  const [loadingModel, setLoadingModel] = useState(true);
   const poseLandmarkerRef = useRef<PoseLandmarker>(null);
   const [size, setSize] = useState({ width: 0, height: 0 });
 
@@ -22,11 +22,11 @@ export default function Home() {
   useEffect(() => {
     setLoadingModel(true);
     const createPoseLandmarker = async () => {
-      const vision = await FilesetResolver.forVisionTasks(`${window.location.href}wasm`);
+      const vision = await FilesetResolver.forVisionTasks(`${window.location.href}/wasm`);
       poseLandmarkerRef.current = await PoseLandmarker.createFromOptions(vision, {
         baseOptions: {
-          modelAssetPath: `${window.location.href}pose_landmarker_lite.task`,
-          delegate: 'GPU',
+          modelAssetPath: `${window.location.href}/pose_landmarker_full.task`,
+          delegate: 'CPU',
         },
         runningMode: 'VIDEO',
         numPoses: 1, // 设置同时检测的最大姿态数量
---
+++ src/app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div className="flex justify-center items-center h-screen overflow-hidden">
+      <div className="text-2xl">测试页面</div>
+    </div>
+  );
+}
