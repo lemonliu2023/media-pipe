d7000bb4b273fbaac70c1cc0117751a7e9fb6238
刘明 <mingliu63@creditease.cn>
2025-04-24 17:58:52 +0800

    fix: build problem

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/media-pipe',
-  assetPrefix: '/media-pipe',
   output: 'export',
-  distDir: 'docs'
 };
 
 export default nextConfig;
