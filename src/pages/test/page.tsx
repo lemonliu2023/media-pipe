179bf17044bb27caef8ec2ac062001d93f240295
刘明 <mingliu63@creditease.cn>
2025-04-25 13:26:54 +0800

    feat: updaet

--- src/pages/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="flex justify-center items-center h-screen overflow-hidden">
-      <div className="text-2xl">测试页面</div>
-    </div>
-  );
-}
